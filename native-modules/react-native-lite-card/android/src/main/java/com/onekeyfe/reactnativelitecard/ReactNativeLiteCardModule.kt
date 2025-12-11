06e2b436a8b045af5f2af4d53da0bc8ae9cabd4a
huhuanming <huanming@onekey.so>
2025-12-12 01:52:16 +0800

    fix: fix build

--- native-modules/react-native-lite-card/android/src/main/java/com/onekeyfe/reactnativelitecard/ReactNativeLiteCardModule.kt
+++ native-modules/react-native-lite-card/android/src/main/java/com/onekeyfe/reactnativelitecard/ReactNativeLiteCardModule.kt
@@ -1,5 +1,6 @@
 package com.onekeyfe.reactnativelitecard
 
+import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 
@@ -11,10 +12,48 @@ class ReactNativeLiteCardModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun getLiteInfo(callback: Callback?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun checkNFCPermission(callback: Callback?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setMnemonic(
+    mnemonic: String?,
+    pwd: String?,
+    overwrite: Boolean,
+    callback: Callback?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getMnemonicWithPin(
+    pwd: String?,
+    callback: Callback?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun changePin(
+    oldPin: String?,
+    newPin: String?,
+    callback: Callback?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun reset(callback: Callback?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun cancel() {
+    TODO("Not yet implemented")
+  }
+
+  override fun intoSetting() {
+    TODO("Not yet implemented")
   }
 
   companion object {
