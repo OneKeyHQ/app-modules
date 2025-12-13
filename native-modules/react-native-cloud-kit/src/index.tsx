75cab5abb604ff3465d47396fe20bce9b96b800d
huhuanming <huanming@onekey.so>
2025-12-13 19:19:26 +0800

    add cloudkit

--- native-modules/react-native-cloud-kit/src/index.tsx
+++ native-modules/react-native-cloud-kit/src/index.tsx
@@ -1,9 +1,6 @@
 import { NitroModules } from 'react-native-nitro-modules';
-import type { CloudKit } from './CloudKit.nitro';
+import type { CloudKit as CloudKitType } from './CloudKit.nitro';
 
 const CloudKitHybridObject =
-  NitroModules.createHybridObject<CloudKit>('CloudKit');
-
-export function multiply(a: number, b: number): number {
-  return CloudKitHybridObject.multiply(a, b);
-}
+  NitroModules.createHybridObject<CloudKitType>('CloudKit');
+export const CloudKit = CloudKitHybridObject;
