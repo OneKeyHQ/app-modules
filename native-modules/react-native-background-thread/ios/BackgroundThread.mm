c7814a9b457b0b1f1933b7f3d6e4d82b3addea18
huhuanming <huanming@onekey.so>
2025-12-15 01:19:11 +0800

    fix: fix code

--- native-modules/react-native-background-thread/ios/BackgroundThread.mm
+++ native-modules/react-native-background-thread/ios/BackgroundThread.mm
@@ -1,32 +1,9 @@
 #import "BackgroundThread.h"
-#import <React/RCTBridge.h>
-#import <React/RCTBundleURLProvider.h>
-#if __has_include(<ReactAppDependencyProvider/RCTAppDependencyProvider.h>)
-#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
-#endif
-#if __has_include(<ReactAppDependencyProvider/RCTReactNativeFactory.h>)
-#import <ReactAppDependencyProvider/RCTReactNativeFactory.h>
-#endif
-#if __has_include(<ReactAppDependencyProvider/RCTAppDependencyProvider.h>)
-#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
-#elif __has_include("RCTAppDependencyProvider.h")
-#import "RCTAppDependencyProvider.h"
-#endif
-#import "BackgroundRunnerReactNativeDelegate.h"
+#import "BackgroundThreadManager.h"
 
-@interface BackgroundThread ()
-@property (nonatomic, strong) BackgroundReactNativeDelegate *reactNativeFactoryDelegate;
-@property (nonatomic, strong) RCTReactNativeFactory *reactNativeFactory;
-@property (nonatomic, assign) BOOL hasListeners;
-@property (nonatomic, assign) BOOL isStarted;
-@end
 
 @implementation BackgroundThread
 
-static BackgroundThread *sharedInstance = nil;
-static BOOL isStarted = NO;
-static NSString *const MODULE_NAME = @"background";
-static NSString *const MODULE_DEBUG_URL = @"http://localhost:8082/apps/mobile/background.bundle?platform=ios&dev=true&lazy=false&minify=false&inlineSourceMap=false&modulesOnly=false&runModule=true&excludeSource=true&sourcePaths=url-server&app=so.onekey.wallet&transform.routerRoot=app&transform.engine=hermes&transform.bytecode=1&unstable_transformProfile=hermes-stable";
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
@@ -35,35 +12,23 @@ static NSString *const MODULE_DEBUG_URL = @"http://localhost:8082/apps/mobile/ba
 }
 
 - (void)startBackgroundRunner {
-  [self startBackgroundRunnerWithEntryURL:MODULE_DEBUG_URL];
+    [[BackgroundThreadManager sharedInstance] startBackgroundRunner];
 }
 
 - (void)startBackgroundRunnerWithEntryURL:(NSString *)entryURL {
-  if (isStarted) {
-      return;
-    }
-    isStarted = YES;
+    BackgroundThreadManager *manager = [BackgroundThreadManager sharedInstance];
     
-    dispatch_async(dispatch_get_main_queue(), ^{
-      NSDictionary *initialProperties = @{};
-      NSDictionary *launchOptions = @{};
-      self.reactNativeFactoryDelegate = [[BackgroundReactNativeDelegate alloc] init];
-      self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self.reactNativeFactoryDelegate];
-      #if DEBUG
-        [self.reactNativeFactoryDelegate setJsBundleSource:std::string([entryURL UTF8String])];
-      #endif
-      [self.reactNativeFactory.rootViewFactory viewWithModuleName:MODULE_NAME
-                                                               initialProperties:initialProperties
-                                                                   launchOptions:launchOptions];
-      __weak __typeof__(self) weakSelf = self;
-      [self.reactNativeFactoryDelegate setOnMessageCallback:^(NSString *message) {
-          [weakSelf emitOnBackgroundMessage:message];
-      }];
-    });
+    // Set up message callback to bridge it back through this instance
+    __weak __typeof__(self) weakSelf = self;
+    [manager setOnMessageCallback:^(NSString *message) {
+        [weakSelf emitOnBackgroundMessage:message];
+    }];
+    
+    [manager startBackgroundRunnerWithEntryURL:entryURL];
 }
 
 - (void)postBackgroundMessage:(nonnull NSString *)message {
-  [self.reactNativeFactoryDelegate postMessage:std::string([message UTF8String])];
+    [[BackgroundThreadManager sharedInstance] postBackgroundMessage:message];
 }
 
 + (NSString *)moduleName
