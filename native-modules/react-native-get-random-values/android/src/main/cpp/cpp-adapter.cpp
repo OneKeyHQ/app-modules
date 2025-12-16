26aa91f4db40bb5fb52999171e6fc4dec30c3701
huhuanming <huanming@onekey.so>
2025-12-16 17:06:05 +0800

    fix: fix app

--- native-modules/react-native-get-random-values/android/src/main/cpp/cpp-adapter.cpp
+++ native-modules/react-native-get-random-values/android/src/main/cpp/cpp-adapter.cpp
@@ -1,5 +1,5 @@
 #include <jni.h>
-#include "HybridReactNativeGetRandomValuesSpecSwift.hpp"
+#include "reactnativegetrandomvaluesOnLoad.hpp"
 
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return margelo::nitro::reactnativegetrandomvalues::initialize(vm);
