1a5d7c3970c713fbf534310fe62f87c48ff93c26
huhuanming <huanming@onekey.so>
2025-12-18 17:58:51 +0800

    init react-native-skeleton

---
+++ native-views/react-native-skeleton/src/Skeleton.nitro.ts
@@ -0,0 +1,15 @@
+import type {
+  HybridView,
+  HybridViewMethods,
+  HybridViewProps,
+} from 'react-native-nitro-modules';
+
+export interface SkeletonProps extends HybridViewProps {
+  color: string;
+}
+export interface SkeletonMethods extends HybridViewMethods {}
+
+export type Skeleton = HybridView<
+  SkeletonProps,
+  SkeletonMethods
+>;
---
+++ native-views/react-native-skeleton/src/index.tsx
@@ -0,0 +1,11 @@
+import { getHostComponent } from 'react-native-nitro-modules';
+const SkeletonConfig = require('../nitrogen/generated/shared/json/SkeletonConfig.json');
+import type {
+  SkeletonMethods,
+  SkeletonProps,
+} from './Skeleton.nitro';
+
+export const SkeletonView = getHostComponent<
+  SkeletonProps,
+  SkeletonMethods
+>('Skeleton', () => SkeletonConfig);
