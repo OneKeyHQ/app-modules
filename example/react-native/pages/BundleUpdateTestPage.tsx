ba2c87905f4f0d3e4af5d55360c981ab68f169a7
huhuanming <huanming@onekey.so>
2026-03-03 15:09:37 +0800

    Update BundleUpdateTestPage.tsx

--- example/react-native/pages/BundleUpdateTestPage.tsx
+++ example/react-native/pages/BundleUpdateTestPage.tsx
@@ -269,13 +269,13 @@ Ni3q+jg=
         latestVersion: '5.19.0',
         bundleVersion: '2',
         fileSize: 55586119,
-        sha256: 'c0beb980fc113bc21ea510b778933ed488dc685c2216105bd146df8e9f791a3d',
+        sha256: 'cdff9d4b37f2940e5ce73141e384f412e1e4e4bf3046b0cdb4d79b0c974de742',
         signature: `-----BEGIN PGP SIGNED MESSAGE-----
 Hash: SHA256
 
 {
   "fileName": "metadata.json",
-  "sha256": "cdff9d4b37f2940e5ce73141e384f412e1e4e4bf3046b0cdb4d79b0c974de742",
+  "sha256": "cece2fccea3c3a43e0da7ab803f44e5a4850a8b06f2df06db3e7f16860080a40",
   "size": 28919,
   "generatedAt": "2026-01-06T05:42:35.407Z",
   "appType": "android",
