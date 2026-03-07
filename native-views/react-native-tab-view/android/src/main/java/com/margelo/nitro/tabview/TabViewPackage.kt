65a144b8a03f68d25e274fc092a7d5e9fdbf6d13
huhuanming <huanming@onekey.so>
2026-03-07 20:01:56 +0800

    Update TabViewPackage.kt

--- native-views/react-native-tab-view/android/src/main/java/com/margelo/nitro/tabview/TabViewPackage.kt
+++ native-views/react-native-tab-view/android/src/main/java/com/margelo/nitro/tabview/TabViewPackage.kt
@@ -4,6 +4,10 @@ import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.facebook.react.uimanager.ViewManager
+
+import com.margelo.nitro.tabview.views.HybridTabViewManager
+import com.margelo.nitro.tabview.views.HybridBottomAccessoryViewManager
 
 class TabViewPackage : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
@@ -12,6 +16,10 @@ class TabViewPackage : BaseReactPackage() {
 
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
         return ReactModuleInfoProvider { HashMap() }
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf(HybridTabViewManager(), HybridBottomAccessoryViewManager())
     }
 
     companion object {
